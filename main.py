d2a31ae345083c8e5ea465a4c87e91623a45d835
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-11-23 12:40:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def echo(client: pyrogram.client.Client, message: pyrogram.types.messages_and_me
         return
 
     app.send_message(message.chat.id, '**Hi, I am Mdisk Video Downloader, you can watch Videos without MX Player.\n__Send me a link to Start...__**',reply_to_message_id=message.id,
-    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Source Code", url="https://github.com/bipinkrish/Mdisk-Downloader-Bot")]]))
+    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Source Code", url="https://github.com/Dkmovie/Mdisk-download-bot")]]))
 
 # help command
 @app.on_message(filters.command(["help"]))
